8bfb16687ce2dd99253aa629886a260edafe017d
thaint <n.thanhthai3010@hotmail.com>
2019-06-27 00:41:27 +0700

    fix syntax

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const pgClient = new Pool({
 pgClient.on("error", () => console.log("Lost PG Connection"));
 
 pgClient
-  .query("CREATE TABLE IF NOT EXIST values (number INT)")
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
   .catch(err => console.log(err));
 
 // redis setup
