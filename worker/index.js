60572687173314413748e75481eb7b2f93e80899
thaint <thaint@blueseed.tv>
2019-06-27 17:13:28 +0700

    update docker-compose fix bug worker service environment

--- worker/index.js
+++ worker/index.js
@@ -14,7 +14,6 @@ function fib(index) {
 }
 
 sub.on("message", (channel, message) => {
-  console.log("Insert new value of " + message);
   redisClient.hset("values", message, fib(parseInt(message)));
 });
 sub.subscribe("insert");
